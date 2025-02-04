97660161025e09ad2ff09d2a532e9df75b6c8505
kayyza <rachel.herron@live.ca>
2025-02-04 11:14:24 -0500

    Created Task and TaskList classes

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Task {
+    private String taskName;
+    private String priority;
+    private boolean completed;
+
+    public Task(String taskName, String priority, boolean completed) {
+        this.taskName = taskName;
+        this.priority = priority;
+        this.completed = false;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskName='" + taskName + '\'' +
+                ", priority='" + priority + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+}
